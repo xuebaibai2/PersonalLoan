33fa2d88642fa47415ec933982a378845e56bcaf
Cayden Li <xuebaibai2@hotmail.com>
2018-11-23 15:26:45 +1100

    init api projcet

---
+++ API/PersonalLoanAPI/PersonalLoanAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PersonalLoanAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ API/PersonalLoanAPI/PersonalLoanAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PersonalLoanAPI.WebApiApplication" Language="C#" %>
