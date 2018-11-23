8486fecdcdcc21807201bb5e0b306aa4aa3b68cb
Cayden Li <xuebaibai2@hotmail.com>
2018-11-23 12:37:45 +1100

    add app state change subscription on the button
    Apply for Increased Loan Amounts button should be enabled only when selected balance is more than 0.

--- UI/PersonalLoanUI/src/app/components/btn-apply-amount/btn-apply-amount.component.ts
+++ UI/PersonalLoanUI/src/app/components/btn-apply-amount/btn-apply-amount.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Store } from '@ngrx/store';
+
+import { Loan } from 'src/app/models/loan.model';
+import * as fromStore from '../../store';
+
 @Component({
   selector: 'app-btn-apply-amount',
   templateUrl: './btn-apply-amount.component.html',
@@ -7,9 +12,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BtnApplyAmountComponent implements OnInit {
 
-  constructor() { }
+  noPayoutAmount: boolean;
+
+  constructor(private store: Store<fromStore.AppState>) { }
 
   ngOnInit() {
+    this.store.select<any>(fromStore.getSelectedLoans).subscribe(
+      (selectedLoans: Loan[]) => {
+        this.noPayoutAmount = selectedLoans.length === 0;
+      }
+    );
   }
-
 }
